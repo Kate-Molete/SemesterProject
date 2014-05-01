7ae12ad94392697ede17bfa571c22ee93a14963b
MyProject <cd MyProject>
2014-05-01 13:04:35 +0200

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SemesterProject::Application.config.secret_token = 'd20b27c4226001a2be01207c6ab033d32c50000747ce9f0ddd924fbce1e5f03a76a7998379926c20bc1379ce1aa6dfc12668db66d14fab64c31ef6a950644308'
